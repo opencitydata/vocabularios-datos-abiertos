c3171f8613331dd23065f2b18fae3856f7454206
Oscar Corcho <ocorcho@fi.upm.es>
2018-07-30 17:52:40 +0200

    Modificaciones realizadas para eliminar elementos que han sido transferidos a sus repositorios

---
+++ generateOntologyDoc.sh
@@ -0,0 +1,28 @@
+#java -jar /Users/ocorcho1/Trabajo/proyectos/src_oeg/widoco/JAR/widoco-0.0.1-jar-with-dependencies.jar -ontFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/0.1/alojamiento.owl -outFolder /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/0.1 -confFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/config/configAlojamiento.properties
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/config/sections/abstract.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/config/sections/description.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/config/sections/introduction.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/config/sections/references.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/alojamiento/0.1/sections
+
+#java -jar /Users/ocorcho1/Trabajo/proyectos/src_oeg/widoco/JAR/widoco-0.0.1-jar-with-dependencies.jar -ontFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/0.1/agenda.owl -outFolder /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/0.1 -confFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/config/configAgenda.properties
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/config/sections/abstract.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/config/sections/description.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/config/sections/introduction.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/config/sections/references.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/cultura-ocio/agenda/0.1/sections
+
+#java -jar /Users/ocorcho1/Trabajo/proyectos/src_oeg/widoco/JAR/widoco-0.0.1-jar-with-dependencies.jar -ontFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/lugar/0.1/lugar.owl -outFolder /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/lugar/0.1 -confFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/lugar/config/configlugar.properties
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/lugar/config/sections/abstract.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/lugar/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/lugar/config/sections/introduction.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/lugar/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/lugar/config/sections/references.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/turismo/lugar/0.1/sections
+
+#java -jar /Users/ocorcho1/Trabajo/proyectos/src_oeg/widoco/JAR/widoco-0.0.1-jar-with-dependencies.jar -ontFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/0.1/subvencion.owl -outFolder /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/0.1 -confFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/config/configSubvenciones.properties
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/config/sections/abstract.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/config/sections/introduction.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/config/sections/description.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/0.1/sections
+#cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/config/sections/references.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/sector-publico/subvencion/0.1/sections
+
+java -jar /Users/ocorcho1/Trabajo/proyectos/src_oeg/widoco/JAR/widoco-0.0.1-jar-with-dependencies.jar -ontFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/0.1/trafico.owl -outFolder /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/0.1 -confFile /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/config/configTrafico.properties
+cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/config/sections/abstract.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/0.1/sections
+cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/config/sections/introduction.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/0.1/sections
+cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/config/sections/description.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/0.1/sections
+cp /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/config/sections/references.html /Users/ocorcho1/Trabajo/proyectos/datosabiertos/transporte/trafico/0.1/sections
